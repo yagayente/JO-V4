88ea7482b0b4aef6837000a8baaac22adf346167
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-09-28 00:06:44 +0200

    404

--- 404.php
+++ 404.php
@@ -1,9 +1,5 @@
-<?php get_header(); ?>
-
-
-		<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dank-portfolio' ); ?></h1>
-				</header><!-- .page-header -->
-
 <?php
-get_footer();
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: ".get_bloginfo('url'));
+exit();
+?>

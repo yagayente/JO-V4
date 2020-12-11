0f151f2f86747b3bf5cb03edae5953215749f450
Simon Bouchard <simonbouchardpro@gmail.com>
2020-12-11 09:40:49 +0100

    corrections

--- template-parts/menu_infos.php
+++ template-parts/menu_infos.php
@@ -5,7 +5,7 @@
   <div class="right">
         <ul id="menu" class="Menu_background">
         <li><a id="showButton" onmouseover="changeColorIn_work()"  onmouseout="changeColorIn_work()" class="nav-link work" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Work</a></h1></li>
-        <li class="current-menu-item"><a id="showButton" onmouseover="changeColorIn_infos()"  onmouseout="changeColorOut_infos()" class="nav-link work" href="http://localhost:8888/7-JOHL/page-d-exemple/" rel="home">Infos</a></h1></li>
+        <li class="current-menu-item"><a id="showButton" onmouseover="changeColorIn_infos()"  onmouseout="changeColorOut_infos()" class="nav-link work" href="<?php echo esc_url( get_permalink( get_page_by_title( 'infos' ) ) ); ?>" rel="home">Infos</a></h1></li>
         </ul>
     </div>
   </nav>
